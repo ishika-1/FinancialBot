5d25b9663f04a1e3c3dd00045a031ca0e2a9cb99
gargdevanshi1 <54847892+gargdevanshi1@users.noreply.github.com>
2020-10-15 23:50:08 +0530

    Update urls.py

--- FinBot/urls.py
+++ FinBot/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
 
 from . import views
 
@@ -9,4 +11,12 @@ urlpatterns = [
         path('registration/',views.registration,name='registration'),
         path('userlogout/',views.userlogout,name="userlogout"),
         path('chart/',views.chart, name="chart"),
+        url(r'^password_reset/$', auth_views.PasswordResetView.as_view(),
+                {'template_name': "registration/password_reset_form.html"}, name='password_reset'),
+        url(r'^password_reset/done/$',auth_views.PasswordResetDoneView.as_view(),
+                {'template_name': "registration/password_reset_done.html"}, name='password_reset_done'),
+        url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(),
+                {'template_name': "registration/password_reset_confirm.html"}, name='password_reset_confirm'),
+        url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(),
+                {'template_name': "registration/password_reset_complete.html"}, name='password_reset_complete'),
 ]
